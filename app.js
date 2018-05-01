7ac51f04408c9b2a772ff21c176a737a8f77c90c
Sylvance <kerandisylvance@gmail.com>
2018-05-01 14:20:13 +0300

    Bind sockets to express server

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
+
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 app.use(index);
+
 const server = http.createServer(app);
 const io = socketIo(server);
-const getApiAndEmit = "TODO"
 
 let interval;
 io.on("connection", socket => {
@@ -21,3 +22,16 @@ io.on("connection", socket => {
         console.log("Client disconnected");
     });
 });
+
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get(
+            "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
+        ); // Getting the data from DarkSky
+        socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
